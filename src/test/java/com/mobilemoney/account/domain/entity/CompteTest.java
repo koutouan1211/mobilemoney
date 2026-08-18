8ab6e8ac580b2d1211cdcd6b77040a373be84411
koutouan1211 <lyndakoutouan@gmail.com>
2026-08-18 10:04:10 +0000

    Initialisation du projet spring boot et creation de test unitaire

--- src/test/java/com/mobilemoney/account/domain/entity/CompteTest.java
+++ src/test/java/com/mobilemoney/account/domain/entity/CompteTest.java
@@ -4,7 +4,9 @@ import com.mobilemoney.account.domain.enums.Profil;
 import com.mobilemoney.account.domain.enums.StatutCompte;
 import com.mobilemoney.account.domain.enums.TypePersonne;
 import com.mobilemoney.account.domain.valueobject.Money;
+import com.mobilemoney.account.domain.valueobject.MotDePasse;
 import com.mobilemoney.account.domain.valueobject.NumeroTelephone;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,41 +16,90 @@ class CompteTest {
     @Test
     void should_create_subscriber_account() {
 
+        MotDePasse motDePasse =
+                MotDePasse.of("1234");
+
         Compte compte = Compte.creerSubscriber(
                 "KOUTOUAN",
                 "Lynda",
                 NumeroTelephone.of("0707070707"),
+                motDePasse,
                 TypePersonne.PERSONNE_PHYSIQUE
         );
 
-        assertEquals(Profil.SUBSCRIBER, compte.getProfil());
-        assertEquals(StatutCompte.ACTIF, compte.getStatut());
-        assertEquals(Money.of("200000"), compte.getPlafond());
-        assertEquals(Money.zero(), compte.getSolde());
+        assertEquals(
+                Profil.SUBSCRIBER,
+                compte.getProfil()
+        );
+
+        assertEquals(
+                StatutCompte.ACTIF,
+                compte.getStatut()
+        );
+
+        assertEquals(
+                Money.of("200000"),
+                compte.getPlafond()
+        );
+
+        assertEquals(
+                Money.zero(),
+                compte.getSolde()
+        );
+
+        assertEquals(
+                motDePasse,
+                compte.getMotDePasse()
+        );
     }
 
-    
-    
+
     @Test
     void should_create_agent_account() {
+
+        MotDePasse motDePasse =
+                MotDePasse.of("1234");
 
         Compte compte = Compte.creerAgent(
                 "Orange",
                 "Money",
                 NumeroTelephone.of("0700000001"),
+                motDePasse,
                 TypePersonne.PERSONNE_MORALE
         );
 
-        assertEquals(Profil.AGENT, compte.getProfil());
-        assertEquals(StatutCompte.EN_ATTENTE, compte.getStatut());
-        assertEquals(Money.zero(), compte.getPlafond());
-        assertEquals(Money.zero(), compte.getSolde());
+        assertEquals(
+                Profil.AGENT,
+                compte.getProfil()
+        );
 
+        assertEquals(
+                StatutCompte.EN_ATTENTE,
+                compte.getStatut()
+        );
+
+        assertEquals(
+                Money.zero(),
+                compte.getPlafond()
+        );
+
+        assertEquals(
+                Money.zero(),
+                compte.getSolde()
+        );
+
+        assertEquals(
+                motDePasse,
+                compte.getMotDePasse()
+        );
     }
-    
-    
+
+
     @Test
     void should_throw_exception_when_subscriber_is_company() {
+
+        MotDePasse motDePasse =
+                MotDePasse.of("1234");
 
         assertThrows(
                 IllegalArgumentException.class,
@@ -56,20 +107,24 @@ class CompteTest {
                         "Entreprise",
                         "XYZ",
                         NumeroTelephone.of("0707070707"),
+                        motDePasse,
                         TypePersonne.PERSONNE_MORALE
                 )
         );
-
     }
-    
-    
+
+
     @Test
     void should_create_company_agent() {
+
+        MotDePasse motDePasse =
+                MotDePasse.of("1234");
 
         Compte compte = Compte.creerAgent(
                 "Entreprise",
                 "XYZ",
                 NumeroTelephone.of("0700000002"),
+                motDePasse,
                 TypePersonne.PERSONNE_MORALE
         );
 
@@ -77,17 +132,20 @@ class CompteTest {
                 TypePersonne.PERSONNE_MORALE,
                 compte.getTypePersonne()
         );
-
     }
-    
-    
+
+
     @Test
     void should_create_physical_agent() {
+
+        MotDePasse motDePasse =
+                MotDePasse.of("1234");
 
         Compte compte = Compte.creerAgent(
                 "Jean",
                 "Kouassi",
                 NumeroTelephone.of("0700000003"),
+                motDePasse,
                 TypePersonne.PERSONNE_PHYSIQUE
         );
 
@@ -95,61 +153,91 @@ class CompteTest {
                 TypePersonne.PERSONNE_PHYSIQUE,
                 compte.getTypePersonne()
         );
-
     }
-    
+
+
     @Test
     void should_credit_account() {
 
+        MotDePasse motDePasse =
+                MotDePasse.of("1234");
+
         Compte compte = Compte.creer(
-        		Profil.SUBSCRIBER,
+                Profil.SUBSCRIBER,
                 "KOUTOUAN",
                 "Lynda",
                 NumeroTelephone.of("0707070707"),
-                TypePersonne.PERSONNE_PHYSIQUE);
+                motDePasse,
+                TypePersonne.PERSONNE_PHYSIQUE
+        );
 
-        compte.crediter(Money.of(10000));
+        compte.crediter(
+                Money.of(10000)
+        );
 
         assertEquals(
                 Money.of(10000),
-                compte.getSolde());
+                compte.getSolde()
+        );
     }
-    
+
+
     @Test
     void should_debit_account() {
 
+        MotDePasse motDePasse =
+                MotDePasse.of("1234");
+
         Compte compte = Compte.creer(
-        		Profil.SUBSCRIBER,
+                Profil.SUBSCRIBER,
                 "KOUTOUAN",
                 "Lynda",
                 NumeroTelephone.of("0707070707"),
-                TypePersonne.PERSONNE_PHYSIQUE);
+                motDePasse,
+                TypePersonne.PERSONNE_PHYSIQUE
+        );
 
-        compte.crediter(Money.of(20000));
-        compte.debiter(Money.of(5000));
+        compte.crediter(
+                Money.of(20000)
+        );
+
+        compte.debiter(
+                Money.of(5000)
+        );
 
         assertEquals(
                 Money.of(15000),
-                compte.getSolde());
+                compte.getSolde()
+        );
     }
-    
+
+
     @Test
     void should_throw_exception_when_balance_is_insufficient() {
 
+        MotDePasse motDePasse =
+                MotDePasse.of("1234");
+
         Compte compte = Compte.creer(
-        		 Profil.SUBSCRIBER,
+                Profil.SUBSCRIBER,
                 "KOUTOUAN",
                 "Lynda",
                 NumeroTelephone.of("0707070707"),
-                TypePersonne.PERSONNE_PHYSIQUE);
+                motDePasse,
+                TypePersonne.PERSONNE_PHYSIQUE
+        );
 
         IllegalArgumentException exception =
                 assertThrows(
                         IllegalArgumentException.class,
-                        () -> compte.debiter(Money.of(1000)));
+                        () -> compte.debiter(
+                                Money.of(1000)
+                        )
+                );
 
         assertEquals(
                 "Solde insuffisant.",
-                exception.getMessage());
+                exception.getMessage()
+        );
     }
 }
